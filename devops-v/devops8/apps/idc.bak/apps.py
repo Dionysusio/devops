c8334711b84cd27cf44c35cec6c00f3392753ec0
amy <eyzk_1112@163.com>
2018-12-23 01:20:14 -0500

    rm

--- devops-v/devops8/apps/idc.bak/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IdcConfig(AppConfig):
-    name = 'idc'
--- devops-v/devops8/apps/resources/qcloud/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-#coding:utf-8
-
-from django.conf  import settings
-from tencentcloud.common import credential
-
-
-def getCredential():
-    return credential.Credential(settings.QCLOUD_SECRETID,settings.QCLOUD_SECRETKEY)
